e70305e70d2d3f8e0990b621837c176aa63a10e3
ELGA <algassimousow799@gmail.com>
2026-02-07 22:09:56 +0000

    Creation des routes de Filiére et Tarif

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+
+use App\Http\Controllers\FiliereController;
+use App\Http\Controllers\TarifController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('filieres', FiliereController::class);
+Route::resource('tarifs', TarifController::class);
